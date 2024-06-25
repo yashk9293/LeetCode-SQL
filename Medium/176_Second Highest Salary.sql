1f6654605a0f14ec8e00c3e2a3954fa099fc1adf
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-06-25 11:09:02 +0530

    Create 176_Second Highest Salary.sql

---
+++ Medium/176_Second Highest Salary.sql
@@ -0,0 +1,25 @@
+-- Question Link :- https://leetcode.com/problems/second-highest-salary/
+-- Second Highest Salary
+
+-- Write a SQL query to get the second highest salary from the Employee table.
+
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- For example, given the above Employee table, the query should return 200 as the second highest salary. 
+-- If there is no second highest salary, then the query should return null.
+
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+
+
+-- Solution
+SELECT MAX(salary) as SecondHighestSalary from Employee 
+where salary < (SELECT MAX(Salary) from Employee);
